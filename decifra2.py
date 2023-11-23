e8a1a62269576ba098b8590ab1bdd66c7d5c4cfe
FMM28 <marquez.maya.francisco.28@gmail.com>
2023-11-22 20:20:40 -0600

    kdwviu

--- decifra2.py
+++ decifra2.py
@@ -13,23 +13,23 @@ def carga(nombre):
 posA = "abcdefghijklmnopqrstuvwxyz"
 posB = "abcdefghijklmnopqrstuvwxyz"
 posC = "abcdefghijklmnopqrstuvwxyz"
-posD = "abcdefghijklmnopqrstuvwxyz"
-posE = "abcdefghijklmnopqrstuvwxyz"
+posD = "dmctx"
+posE = "tnlpdg"
 posF = "abcdefghijklmnopqrstuvwxyz"
 posG = "abcdefghijklmnopqrstuvwxyz"
 posH = "abcdefghijklmnopqrstuvwxyz"
-posI = "ctsjn"
+posI = "ctsjfbn"
 posJ = "e"
 posK = "abcdefghijklmnopqrstuvwxyz"
 posL = "abcdefghijklmnopqrstuvwxyz"
 posM = "abcdefghijklmnopqrstuvwxyz"
 posN = "abcdefghijklmnopqrstuvwxyz"
 posO = "abcdefghijklmnopqrstuvwxyz"
-posP = "abcdefghijklmnopqrstuvwxyz"
+posP = "aiuo"
 posQ = "abcdefghijklmnopqrstuvwxyz"
-posR = "abcdefghijklmnopqrstuvwxyz"
+posR = "pvhflcmgsbnr"
 posS = "abcdefghijklmnopqrstuvwxyz"
-posT = "trls"
+posT = "trlsm"
 posU = "abcdefghijklmnopqrstuvwxyz"
 posV = "abcdefghijklmnopqrstuvwxyz"
 posW = "abcdefghijklmnopqrstuvwxyz"
@@ -39,7 +39,7 @@ posZ = "abcdefghijklmnopqrstuvwxyz"
 
 dicFr=carga('dicFr.txt')
 dicFr=list(set(dicFr))
-print(len(dicFr))
+# print(len(dicFr))
 
 #igual que el texto 1 deducimos que la j es una e por ser la que tiene mayor frecuencia
 
@@ -47,8 +47,8 @@ c=0
 
 
 #ijttj
-#posT=rlts
-#posI=ctsjn
+#posT=rltsm
+#posI=ctsjfbn
 # for p in dicFr:
 #       if len(p)==5:
 #             if p[1]==p[4] and p[2]==p[3] and p[1] in posJ:
@@ -56,8 +56,15 @@ c=0
 #                   print(p)
 
 #rpttj
+# for p in dicFr:
+#       if len(p)==5:
+#             if p[2]==p[3] and p[2] in posT and p[-1] in posJ and p[1]!=p[-1]:
+#                   c+=1
+#                   print(p)
+
+#jdpej
 for p in dicFr:
       if len(p)==5:
-            if p[2]==p[3] and p[2] in posT and p[-1] in posJ:
-                  c+=1
+            if p[0]==p[-1] and p[0] in posJ and p[2] in posP:
                   print(p)
+
